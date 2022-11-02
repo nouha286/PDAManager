7783f7595850fc1a34aa9cdd13765a810e877f80
hamzacos <hamza.laqraa@hotmail.com>
2022-11-02 14:04:41 +0100

    First commit in develop

---
+++ TestPDA/src/main/java/Main.java
@@ -0,0 +1,14 @@
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		EntityManager em=Persistence.createEntityManagerFactory("TestPDA").createEntityManager();
+		System.out.println("done");
+
+	}
+
+}
